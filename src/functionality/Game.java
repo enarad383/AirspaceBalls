57566d3ca1b0d2d289b0627bc5535f8e1bfd8592
enarad383 <enarad383@PR37.hhs.win.fuhsd.org>
2017-05-09 09:49:03 -0700

    Started coding. Window now opens.

---
+++ src/functionality/Game.java
@@ -0,0 +1,16 @@
+package functionality;
+
+import processing.core.PApplet;
+
+public class Game extends PApplet{
+	public Game(){
+		super();
+		runSketch();
+	}
+	public void setup(){
+		
+	}
+	public void draw(){
+		
+	}
+}
---
+++ src/functionality/Main.java
@@ -0,0 +1,25 @@
+package functionality;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//drawing.runSketch(); //removed because runSketch can only be called from PApplet
+		Game game = new Game();
+		PSurfaceAWT surf = (PSurfaceAWT) game.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+		window.setMinimumSize(new Dimension(100,100));
+		window.setSize(1920, 1000);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+	}
+
+}
