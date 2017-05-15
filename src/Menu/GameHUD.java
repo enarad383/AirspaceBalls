b17af4e963f5854d461714a23801ceb8dcab87bd
bchun141 <bchun141@PR38.hhs.win.fuhsd.org>
2017-05-15 09:15:26 -0700

    deleted stuff and organized to the uml thingy

--- src/Menu/GameHUD.java
+++ src/Menu/GameHUD.java
@@ -5,72 +5,25 @@ import java.awt.geom.AffineTransform;
 
 import javax.swing.*;
 
+import functionality.Game;
 import functionality.Main;
 
 import java.util.*;
 
 
-public class GameHUD extends JPanel implements KeyListener
+public class GameHUD extends Game 
 {
 
-	private String message;
-	private Main m;
-
-	public GameHUD () {
-		super();
-		//this.m = m;
-		setBackground(Color.WHITE);
-		message = "This is the game screen! Press escape to quit.";
-		System.out.println("HHHLKJ");
-	}
+	
+	
 
 
 	public void paintComponent(Graphics g)
 	{
-		super.paintComponent(g);  // Call JPanel's paintComponent method to paint the background
-
-		Graphics2D g2 = (Graphics2D)g;
-
-		int width = getWidth();
-		int height = getHeight();
-
-		double ratioX = (double)width/800.0;
-		double ratioY = (double)height/600.0;
-
-		AffineTransform at = g2.getTransform();
-		g2.scale(ratioX, ratioY);
-
-		g.setColor(Color.BLACK);
-		g.setFont(new Font("SansSerif",Font.BOLD,28));
-		int strWidth = g.getFontMetrics().stringWidth(message);
-		g.drawString(message, 400-strWidth/2, 300);
-
-		g2.setTransform(at);
-
-		// TODO Add any custom drawings here
+		
 	}
 
 
-	@Override
-	public void keyPressed(KeyEvent arg0) {
-		if (arg0.getKeyCode() == KeyEvent.VK_ESCAPE) {
-			//m.changePanel("1");
-		}
-	}
-
-
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 
 }
